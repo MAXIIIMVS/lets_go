13e6ac6095ffa36dc8bcc8421142692e1bbdbcda
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-05 19:19:46 +0330

    Create new handlers and add new routes.

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("View an specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", home)
+	mux.HandleFunc("/", home)                    // NOTE: ends with / so a subtree path
+	mux.HandleFunc("/snippet/view", snippetView) // NOTE: doesn't end with / so a fixed path
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Println(err)
