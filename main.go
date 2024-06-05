4482772158214346e69a4e73688f8b2ee18b476a
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-05 19:57:25 +0330

    Handle POST method for /snippet/create route and make sure that view path has an id query parameter.

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
 
-func snippetView(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("View an specific snippet"))
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d\n", id)
 }
 
 func snippetCreate(w http.ResponseWriter, _ *http.Request) {
@@ -21,7 +28,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
 	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreate)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Println(err)
