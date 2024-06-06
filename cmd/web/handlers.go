c0f6a8f9c2ea2b7fbc39bf4239618f0a9c7da423
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-06 16:48:38 +0330

    Display dynamic data. Create a view template to show details of each snippet.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -19,7 +20,7 @@ func (app *application) home(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprintf(w, "%+v\n", snippet)
 	}
 	// files := []string{
-	// 	"./ui/html/pages/base.tmpl",
+	// 	"./ui/html/base.tmpl",
 	// 	"./ui/html/partials/nav.tmpl",
 	// 	"./ui/html/pages/home.tmpl",
 	// }
@@ -49,7 +50,23 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	fmt.Fprintf(w, "%+v", snippet)
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	data := &templateDate{
+		Snippet: snippet,
+	}
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/eXvimmer/lets_go/internal/models"
+
+type templateDate struct {
+	Snippet *models.Snippet
+}
