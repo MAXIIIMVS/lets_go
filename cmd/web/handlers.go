35913e6dfa8ffcfdbc8f7dff58419e51423b15ad
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-07 15:14:44 +0330

    Parse form data and create a snippet.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -51,14 +51,22 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// TODO: FIX: this
-	title := "Dummy title"
-	content := "Dummy content"
-	expires := 7
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil /* || title == "" || content == "" */ {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
