845f0484326424a2cef44872dce86ebf7a63c21f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-07 22:36:56 +0330

    Install and use a form decoder.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/eXvimmer/lets_go/internal/models"
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
@@ -18,6 +19,7 @@ type application struct {
 	infoLog       *log.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -62,11 +64,14 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	srv := &http.Server{
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/eXvimmer/lets_go
 go 1.22.1
 
 require (
+	github.com/go-playground/form/v4 v4.2.1
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/joho/godotenv v1.5.1
 	github.com/justinas/alice v1.2.0
